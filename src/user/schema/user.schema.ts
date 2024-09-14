2018ffdb5885c5d146805947c434b8ef85706e5e
Matheus <matalves12345@gmail.com>
2024-09-14 13:36:03 -0300

    Estrutura inicial de usuário

---
+++ src/user/schema/user.schema.ts
@@ -0,0 +1,18 @@
+import { Document, Types } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class User extends Document {
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+  
+  @Prop({ required: true })
+  password: string;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
